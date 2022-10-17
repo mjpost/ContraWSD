d6661659b2ca901e0f9675e770e84f17429c8062
Matt Post <mattpost@microsoft.com>
2022-10-17 19:17:23 +0000

    added download script

---
+++ bin/download.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p data
+wget -q https://data.statmt.org/ContraWSD/nc11_de_fr_sentence_aligned.tar.bz2
+wget -q https://data.statmt.org/ContraWSD/wsd_testset_corpora_de_en.tar.bz2
+wget -q https://data.statmt.org/ContraWSD/wsd_testset_corpora_de_fr.tar.bz2
